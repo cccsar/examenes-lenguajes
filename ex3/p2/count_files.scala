02ed2b90e1325fd03a69639e76434e9d09642487
cccsar <15-11295@usb.ve>
2021-04-15 22:19:30 -0400

    I'm hopeless

--- ex3/p2/count_files.scala
+++ ex3/p2/count_files.scala
@@ -1,44 +1,36 @@
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.util.{Failure, Success}
-
-
-import java.util.Scanner
 import java.io.File
 import scala.io.StdIn
 
+class MyThread extends Thread { 
+  override def run () { 
+    println (s"I'm ${ Thread.currentThread().getName } ") 
+  }
+}
+
 object Count_files{ 
+
+  var counter : Int = 0
 
   def main(args: Array[String]) { 
 
-    println ("Give me a directory:") ;
+    var xd = new MyThread() 
+    var xp = new MyThread()
 
-    val path : String = readLine() 
-//    val files : File = new File(path)
-    val result  = traverse(path)
-    var cond = true 
+    xd.start()
+    xp.start() 
+
+    println(s"result: ${counter}") 
+
+    //println ("Give me a directory:") ;
+    //val path : String = readLine() 
 
     //files.listFiles.foreach { println } 
-
-    result.onComplete { 
-      case Success(x) => {  
-        println(s"Number of directories ${x}" )
-        cond = false
-      }
-      case Failure(e) => { 
-        println("aqui") 
-        cond = false
-      }
-    }
-
   }
 
-  def traverse(path : String) : Future[Int] = Future { 
-    val thisD : File  = new File(path)
-    var acc : Int = 0 
-
-    acc = thisD.listFiles.filter(_.isDirectory).length 
-    acc
+  def do_something(el : Int) : Unit = { 
+    synchronized { 
+      counter += el  
+    }
   }
 
 }
